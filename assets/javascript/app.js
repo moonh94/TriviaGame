b71e422e8e6fcdee3b5fbde501f459b11a2cb818
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-03-06 23:23:22 -0700

    css and pseudocoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//write out a various questions
+    //answers in a string that are linked to buttons
+//set a timer and have the time show via .text
+//have the timer start with a click event for start. 
+//hide questions until start is pressed
+//or set alert to warn the player the game is about to start.
+//set booleans for the answer choices, increase correct or incorrect
+//when time runs out, show score, no longer can click on the answers
+
+
